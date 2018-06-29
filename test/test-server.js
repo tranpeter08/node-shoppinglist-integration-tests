6ff9bb52ff05e4e11d31dd1b9d15af2424b6744c
Peter Tran <tran.peter08@gmail.com>
2018-06-29 15:16:08 -0700

    added recipe tests

--- test/test-server.js
+++ test/test-server.js
@@ -13,6 +13,59 @@ const expect = chai.expect;
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
+describe("Recipes", function(){
+
+  before(function(){
+    return runServer();
+  });
+
+  after(function(){
+    return closeServer();
+  });
+
+  it("should list recipes on GET", function(){
+    return chai
+    .request(app)
+    .get("/recipes")
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a("array");
+
+      // because we create three items on app load
+      expect(res.body.length).to.be.at.least(1);
+      // each item should be an object with key/value pairs
+      // for `id`, `name` and `checked`.
+      const expectedKeys = ["id","name", "ingredients"];
+      res.body.forEach(function(item) {
+        expect(item).to.be.a("object");
+        expect(item).to.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  it("should add a recipe on POST", function() {
+    const newItem = { name: "water", ingredients: ["h", '2', "o"] };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, { id: res.body.id })
+        );
+      });
+  });
+
+});
+
 describe("Shopping List", function() {
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by

10874742427eecd3f2d0df828acb1a08080dea16
Peter Tran <tran.peter08@gmail.com>
2018-06-29 16:28:43 -0700

    updated tests

--- test/test-server.js
+++ test/test-server.js
@@ -13,6 +13,7 @@ const expect = chai.expect;
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
+//tests for recipes
 describe("Recipes", function(){
 
   before(function(){
@@ -64,6 +65,56 @@ describe("Recipes", function(){
       });
   });
 
+  it("should update recipes on PUT", function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      name: "water",
+      ingredients: ["h","2","o"]
+    };
+
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an idea of object to update
+        .get("/recipes")
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          // this will return a promise whose value will be the response
+          // object, which we can inspect in the next `then` block. Note
+          // that we could have used a nested callback here instead of
+          // returning a promise and chaining with `then`, but we find
+          // this approach cleaner and easier to read and reason about.
+          return chai
+            .request(app)
+            .put(`/recipes/${updateData.id}`)
+            .send(updateData);
+        })
+        // prove that the PUT request has right status code
+        // and returns updated item
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+
+  it("should delete recipes on DELETE", function() {
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an `id` of item
+        // to delete
+        .get("/recipes")
+        .then(function(res) {
+          return chai.request(app).delete(`/recipes/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+  
 });
 
 describe("Shopping List", function() {
@@ -112,6 +163,9 @@ describe("Shopping List", function() {
           expect(item).to.include.keys(expectedKeys);
         });
       });
+
+
+      
   });
 
   // test strategy:
@@ -136,7 +190,8 @@ describe("Shopping List", function() {
           Object.assign(newItem, { id: res.body.id })
         );
       });
-  });
+    })
+  
 
   // test strategy:
   //  1. initialize some update data (we won't have an `id` yet)
